1ac535e021b2dd86fd9aa8604badaf52a9166edc
Kristijan Sekeres <kristijan@c9.rs>
2025-07-13 01:36:24 +0000

    Translated using Weblate (Serbian)
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: Clasora V3/dashboard_cards
    Translate-URL: https://localization.clasora.com/projects/clasora-v3/dashboard_cards/sr/

--- sr/dashboard_cards.php
+++ sr/dashboard_cards.php
@@ -1,38 +1,30 @@
 <?php
-
 return [
-  'discord' => 
-  [
-    'title' => 'Pridružite se našoj zajednici',
-    'description' => 'Razmislite o pridruživanju našoj Discord zajednici. Vaše mišljenje nam može pomoći da stvari učinimo još boljim.',
-    'action' => 'Discord Server',
-  ],
-  'email_verification' => 
-  [
-    'description' => 'Email adresa koju ste uneli nije validna i ne možemo vas kontaktirati. Molimo vas da je ažurirate na vašoj stranici profila i obavestite nas slanjem emaila na:',
-  ],
-  'rate_teacher' => 
-  [
-    'description' => 'Molimo vas da odvojite trenutak da ocenite vaš nedavni čas sa vašim predavačem. Vaši utisci nam pomažu da poboljšamo iskustvo učenja za sve.',
-  ],
-  'feedback' => 
-  [
-    'description' => 'Cenimo vaše mišljenje! Ako mislite da nešto nedostaje ili ste naišli na neki problem, molimo vas da nas obavestite posetom',
-  ],
-  'localization' => 
-  [
-    'description' => 'Oslanjamo se na našu zajednicu prilikom razvoja platforme. Želite da pomognete da prevedemo platformu na što više jezika? Pridružite nam se!',
-    'action' => 'Pridružite nam se na Weblate-u',
-    'title' => 'Učestvujte u prevodu',
-  ],
-  'article_author' => 
-  [
-    'title' => 'Postanite pisac',
-    'description' => 'Istaknite se na mreži sa pametnim, visokokvalitetnim člancima koji pokazuju vaše znanje. Počnite da pišete sada.',
-  ],
-  'create_post' => 
-  [
-    'title' => 'Počnite predavati',
-    'description' => 'Spremni ste da objavite novi oglas? Javite nam tako što ćete popuniti obrazac.',
-  ],
+    'discord' => [
+        'title' => 'Pridružite se našoj zajednici',
+        'description' => 'Razmislite o pridruživanju našoj Discord zajednici. Vaše mišljenje nam može pomoći da stvari učinimo još boljim.',
+        'action' => 'Discord Server',
+    ],
+    'email_verification' => [
+        'description' => 'Email adresa koju ste uneli nije validna i ne možemo vas kontaktirati. Molimo vas da je ažurirate na vašoj stranici profila i obavestite nas slanjem emaila na:',
+    ],
+    'rate_teacher' => [
+        'description' => 'Molimo vas da odvojite trenutak da ocenite vaš nedavni čas sa vašim predavačem. Vaši utisci nam pomažu da poboljšamo iskustvo učenja za sve.',
+    ],
+    'feedback' => [
+        'description' => 'Cenimo vaše mišljenje! Ako mislite da nešto nedostaje ili ste naišli na neki problem, molimo vas da nas obavestite posetom',
+    ],
+    'localization' => [
+        'description' => 'Oslanjamo se na našu zajednicu prilikom razvoja platforme. Želite da pomognete da prevedemo platformu na što više jezika? Pridružite nam se!',
+        'action' => 'Pridružite nam se na Weblate-u',
+        'title' => 'Učestvujte u prevodu',
+    ],
+    'article_author' => [
+        'title' => 'Postanite autor',
+        'description' => 'Istaknite se na mreži sa pametnim, visokokvalitetnim člancima koji pokazuju vaše znanje. Počnite da pišete sada.',
+    ],
+    'create_post' => [
+        'title' => 'Počnite predavati',
+        'description' => 'Spremni ste da objavite novi oglas? Javite nam tako što ćete popuniti obrazac.',
+    ],
 ];
